fe359dc25632382e29cd89620bac329b98607806
cyspatial <mustafaceyhan420@gmail.com>
2023-05-18 06:25:45 +0300

    add_raster function eklendi

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ with col1:
     m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, layers_control=True,minimap_control=True)
     m.add_basemap(basemap)
     m.set_center(35.888432,39.654455)
-    m.add_local_tile(topoPafta,layer_name="bascatak_topo")
+    m.add_raster(topoPafta)
     m.add_shp(bascatak,"bascatak_plan_siniri",style=style_b,info_mode=False)
     m.add_shp(culhali,"culhali_plan_siniri",style=style_c,info_mode=False)
     m.add_search_control(bascatak)
